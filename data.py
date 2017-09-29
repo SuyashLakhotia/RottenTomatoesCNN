9e1082c1d5df9e906820c85bd5ade9115d1a76a3
Suyash Lakhotia <suyashlakhotia@gmail.com>
2017-09-30 00:37:39 +0700

    Minor Fix to Comment

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ from collections import Counter
 
 def clean_str(string):
     """
-    Tokenization/string cleaning for all datasets except for SST.
+    Tokenization & string cleaning for all datasets.
     Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
     """
     string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
