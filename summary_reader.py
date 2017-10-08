ec68f91cedf1ea181514545e0f0b19722c34dd9d
Suyash Lakhotia <suyashlakhotia@gmail.com>
2017-10-08 23:20:18 +0800

    Added Script to Read TF Summaries

---
+++ summary_reader.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+folder = ''
+subfolder = ''
+filename = ''
+path = 'runs/' + folder + '/summaries/' + subfolder + '/' + filename
+
+accuracies = []
+losses = []
+for e in tf.train.summary_iterator(path):
+    for v in e.summary.value:
+        if v.tag == 'accuracy_1':
+            accuracies.append(v.simple_value)
+        if v.tag == 'loss_1':
+            losses.append(v.simple_value)
